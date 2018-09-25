1b2660a516dc21d9ca1d5eee2afc4bdfa3d29c60
Владимир Зубович <moraviin@gmail.com>
2018-09-25 13:45:56 +0300

    4/12 hate this sudoku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function solveSudoku(matrix) {
   let uniqNumbers={1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], count:0};
   let zeros=[];
+  let zerosGroup=[];
   let numbers=[];
   let row,column,district;
-  let copyMatrix=[];
   let k=0;
   let changes=0;
   for(let r=0;r<9;r++){//zero init
@@ -39,33 +39,16 @@ module.exports = function solveSudoku(matrix) {
       numbers.forEach(function(number){
       delete zeros[i][number];})
     }
-
+    for(let r=0; r<9 ; r++){
+      validation(r,"row");
+    }
+    for(let c=0; c<9; c++){
+      validation(c,"column");
+    }
     for(let distr=0; distr<9; distr++){//unique zeros in district check
-      uniqNumbers={1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], count:0};
-      for(let i=0; i<zeros.length; i++){
-        if(zeros[i].district == distr){
-          for(var numb in zeros[i]){
-            if(!isNaN(numb)){ // if numb-integer
-            uniqNumbers[numb].push(i);
-            }
-          }
-          uniqNumbers.count++;
-        }    
-      }
-      if(uniqNumbers.count>1){
-        for(var key in uniqNumbers){
-          if(uniqNumbers[key].length==1){
-           k = uniqNumbers[key][0]; // k - number of zero in zeros_array with uniq variable
-           for(var keyZ in zeros[k]){
-             if(keyZ!=key && !isNaN(keyZ)){
-             delete zeros[k][keyZ];
-             } 
-           }
-         }
-       }
-      }
+      validation(distr,"district")
      }
-
+    
     for(let i=0;i<zeros.length;i++){//solution->matrix
       if(Object.keys(zeros[i]).length==4){
         row=zeros[i].row;
@@ -76,9 +59,121 @@ module.exports = function solveSudoku(matrix) {
         changes++;
       }
     }
+
     if(changes==0){
       return matrix;
     }
   }
-  // your solution
+  function validation(value, group){
+    zerosGroup=[];
+    for(let i=0; i<zeros.length; i++){
+      if(zeros[i][group] == value){
+        zerosGroup.push(i)
+      }    
+    }
+    uniqCheck(zerosGroup);
+   }
+  function uniqCheck (arr){// arr - array of number zeros element in group
+    uniqNumbers={1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], count:0};
+    for(let i=0; i<arr.length; i++){
+      for(var numb in zeros[arr[i]]){
+        if(!isNaN(numb)){ // if numb-integer
+        uniqNumbers[numb].push(arr[i]);
+        }
+      }
+      uniqNumbers.count++;     
+    }
+    hidenOnesCheck();
+    hidenDoublesCheck();
+    nakedDoubles();
+  }
+  function hidenOnesCheck(){
+    if(uniqNumbers.count>1){
+      for(var key in uniqNumbers){
+        if(uniqNumbers[key].length==1){
+         k = uniqNumbers[key][0]; // k - number of zero in zeros_array with uniq variable
+         for(var keyZ in zeros[k]){
+           if(keyZ!=key && !isNaN(keyZ)){
+           delete zeros[k][keyZ];
+           } 
+         }
+       }
+     }
+    }
+  }
+  function hidenDoublesCheck(){
+    let double=[];
+    let firstKey=0;
+    let secondKey=0;
+    if(uniqNumbers.count>2){
+      for(let key in uniqNumbers){
+        if(uniqNumbers[key].length==2){
+         double.push(key); // k - number of zero in zeros_array with uniq variable
+         }
+       }
+      for(let i=0; i<double.length; i++){
+        for(let b=i+1;b<double.length;b++){
+          if(uniqNumbers[double[i]]==uniqNumbers[double[b]]){
+            firstKey=uniqNumbers[double[i]][0];
+            secondKey=uniqNumbers[double[i]][1];
+            deletingDoubles(firstKey,double[i],double[b]);
+            deletingDoubles(secondKey,double[i],double[b]);
+          }
+        }
+     }
+    }
+  }
+  function deletingDoubles(pos,num1,num2){
+    for(var key in zeros[pos]){
+      const firstCondition=(key!=num1||key!=num2);
+      const secondCodition=!isNaN(key);
+      if(firstCondition&&secondCodition){
+        delete zeros[pos][key];
+      }
+    }
+  }
+  function nakedDoubles(){
+    let doubleKeyGroup=[];
+    zeros.forEach(function(value,i){
+      if (Object.keys(value).length==5){ //2key + 3key position
+        doubleKeyGroup.push(i);
+      }
+    });
+    doubleKeyGroup.forEach(function(val,i,arr){//0->end
+      for(let b=i+1, len=arr.length;b<len ;b++){//current->end
+        const condition0=Object.keys(zeros[val])[0]==Object.keys(zeros[arr[b]])[0];
+        const condition1=Object.keys(zeros[val])[1]==Object.keys(zeros[arr[b]])[1];
+        if(condition0 && condition1){
+          if(zeros[val].row==zeros[arr[b]].row){
+            let group=zeros[val].row;
+            let n1=Object.keys(zeros[val])[0];
+            let n2=Object.keys(zeros[val])[1];
+            deletingPairs(n1,n2,"row",group,val,arr[b])
+          }
+          if(zeros[val].column==zeros[arr[b]].column){
+            let group=zeros[val].column;
+            let n1=Object.keys(zeros[val])[0];
+            let n2=Object.keys(zeros[val])[1];
+            deletingPairs(n1,n2,"column",group,val,arr[b])
+          }
+          if(zeros[val].district==zeros[arr[b]].district){
+            let group=zeros[val].district;
+            let n1=Object.keys(zeros[val])[0];
+            let n2=Object.keys(zeros[val])[1];
+            deletingPairs(n1,n2,"district",group,val,arr[b])
+          }
+        }
+      }
+    })
+  }
+  function deletingPairs(num1,num2,group,value,zero1,zero2){
+    for(let i=0; i<zeros.length; i++){
+      const condition1=zeros[i][group] == value;
+      const condition2=i!=zero1&&i!=zero2;
+      if(condition1 && condition2){
+        delete zeros[i][num1];
+        delete zeros[i][num2];
+      }    
+    }
+  }
 }
